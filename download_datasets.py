4dd7865bad9bc5c6e318876b089514ee73a2c4b5
bartmanski <98028442+bartmanski@users.noreply.github.com>
2025-01-18 03:16:55 +0100

    fix to download_datasets and some data structure

--- download_datasets.py
+++ download_datasets.py
@@ -1,6 +1,22 @@
 import kagglehub
+import os
+import shutil
+datasets_path="./Datasets"
 
-# Download latest version
-path = kagglehub.dataset_download("shaunthesheep/microsoft-catsvsdogs-dataset", destination = "./Datasets" )
+# Kotki pieski
+path = kagglehub.dataset_download("shaunthesheep/microsoft-catsvsdogs-dataset" )
 
-print("Path to dataset files:", path)
+if not os.path.exists(datasets_path):
+    os.makedirs(datasets_path)
+
+for file_name in os.listdir(path):
+    shutil.move(os.path.join(path, file_name), datasets_path)
+
+# losowe rzeczy
+path = kagglehub.dataset_download("pankajkumar2002/random-image-sample-dataset")
+
+if not os.path.exists(datasets_path):
+    os.makedirs(datasets_path)
+
+for file_name in os.listdir(path):
+    shutil.move(os.path.join(path, file_name), datasets_path)
